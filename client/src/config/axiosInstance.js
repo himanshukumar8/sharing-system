aba404d34e433003f1487baa980ebf8d6781ccd5
himanshukumar8 <69477742+himanshukumar8@users.noreply.github.com>
2026-03-17 22:22:13 +0530

    fixed guest download + API URLs

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL="http://localhost:6600/api/"
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 const axiosInstance=axios.create()
 
 axiosInstance.defaults.baseURL=BASE_URL;
